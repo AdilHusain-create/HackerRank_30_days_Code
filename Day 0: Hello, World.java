8845567ce9db841d60aab383f9dc54260df8af9c
Adil Husain <adilhusain93@gmail.com>
2021-09-27 19:26:48 +0530

    Day 0 : Java Code

---
+++ Day 0: Hello, World.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        System.out.print("Hello, World.\n"+input1);
+    }
+}
